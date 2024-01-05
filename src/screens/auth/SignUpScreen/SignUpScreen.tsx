4881b61e59b83c85167fca274cd43619b74bc368
Jheferson <jheferson@pop-os.localdomain>
2024-01-05 09:24:02 -0300

    SignUp form validation with zod

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { Screen } from '../../../components/Screen/Screen';
 import { Text } from '../../../components/Text/Text';
-import { TextInput } from '../../../components/TextInput/TextInput';
 import { Button } from '../../../components/Button/Button';
-import { PasswordInput } from '../../../components/PasswordInput/PasswordInput';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../routes/Routes';
-// import { useResetNavigationSuccess } from '../../../hooks/useResetNavigationSuccess';
-import { Controller, useForm } from 'react-hook-form';
+import { useResetNavigationSuccess } from '../../../hooks/useResetNavigationSuccess';
+import { useForm } from 'react-hook-form';
 import { FormTextInput } from '../../../components/Form/FormTextInput';
 import { FormPasswordInput } from '../../../components/Form/FormPasswordTextInput';
-
-type SignUpFormType = {
-  username: string;
-  fullName: string;
-  email: string;
-  password: string;
-};
+import { SignUpSchema, signUpSchema } from './signUpSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SignUpScreen({ navigation }: ScreenProps) {
-  // const { reset } = useResetNavigationSuccess();
-  const { control, formState, handleSubmit } = useForm<SignUpFormType>({
+  const { reset } = useResetNavigationSuccess();
+  const { control, formState, handleSubmit } = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
     defaultValues: {
       username: '',
       fullName: '',
@@ -33,16 +27,15 @@ export function SignUpScreen({ navigation }: ScreenProps) {
     mode: 'onChange',
   });
 
-  function submitForm(formValues: SignUpFormType) {
-    console.log(formValues);
-    // reset({
-    //   title: 'Sua conta foi criada com sucesso!',
-    //   description: 'Agora é só fazer login na nossa plataforma.',
-    //   icon: {
-    //     name: 'checkRound',
-    //     color: 'greenPrimary',
-    //   },
-    // });
+  function submitForm() {
+    reset({
+      title: 'Sua conta foi criada com sucesso!',
+      description: 'Agora é só fazer login na nossa plataforma.',
+      icon: {
+        name: 'checkRound',
+        color: 'greenPrimary',
+      },
+    });
   }
 
   return (
@@ -54,13 +47,6 @@ export function SignUpScreen({ navigation }: ScreenProps) {
       <FormTextInput
         control={control}
         name="username"
-        rules={{
-          required: 'Username é obrigatório',
-          minLength: {
-            value: 3,
-            message: 'Username deve ter no mínimo 3 caracteres',
-          },
-        }}
         placeholder="@"
         label="Seu username"
         boxProps={{ mb: 's20' }}
@@ -69,9 +55,6 @@ export function SignUpScreen({ navigation }: ScreenProps) {
       <FormTextInput
         control={control}
         name="fullName"
-        rules={{
-          required: 'Nome completo é obrigatório',
-        }}
         placeholder="Digite seu nome completo"
         label="Nome completo"
         boxProps={{ mb: 's20' }}
@@ -81,13 +64,6 @@ export function SignUpScreen({ navigation }: ScreenProps) {
       <FormTextInput
         control={control}
         name="email"
-        rules={{
-          required: 'E-mail é obrigatório',
-          pattern: {
-            value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-            message: 'E-mail inválido',
-          },
-        }}
         placeholder="Digite seu e-mail"
         label="E-mail"
         boxProps={{ mb: 's20' }}
@@ -96,13 +72,6 @@ export function SignUpScreen({ navigation }: ScreenProps) {
       <FormPasswordInput
         control={control}
         name="password"
-        rules={{
-          required: 'Senha é obrigatória',
-          minLength: {
-            value: 8,
-            message: 'Senha deve ter no mínimo 8 caracteres',
-          },
-        }}
         placeholder="Digite sua senha"
         label="Senha"
         boxProps={{ mb: 's48' }}
